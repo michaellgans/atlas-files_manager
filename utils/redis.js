aedde844e1a0b182f5eda95327d6cc30f501f333
michaellgans <michaellgans@Victoria>
2024-07-09 15:05:02 -0500

    M - 3:04 PM - Added set, get, and del functions

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,6 @@
 // Task 0 - Redis Utils: Contains the Class Redis Client
 
+import { interfaces } from 'mocha';
 import redis from 'redis';
 
 class RedisClient {
@@ -24,10 +25,46 @@ class RedisClient {
     }
     return false
   }
+
+  // Returns the value of a key
+  async get(key) {
+    try {
+      const value = await this.client.get(key);
+      return value;
+    } catch (err) {
+      console.error('Could not GET value:', err);
+    }
+  };
+
+  // Stores the value by a key
+  async set(key, value, time) {
+    try {
+      await this.client.set(key, value, time);
+      console.log(`Your key ${key} has been set with a value of ${value} and a duration of ${time}.`)
+    } catch (err) {
+      console.error('Could not SET value:', err);
+    }
+  };
+
+  // Deletes a value of a key
+  async del(key) {
+    try {
+      await this.client.del(key);
+      console.log(`Key ${key} has been deleted.`)
+    } catch (err) {
+      console.error('Could not delete key', err);
+    }
+  };
 }
 
 // Testing
-const client = new RedisClient();
-setTimeout(() => {
-  console.log(client.isAlive());
-}, 3000);
+// const client = new RedisClient();
+
+// setTimeout(() => {
+//   console.log(client.isAlive());
+// }, 3000);
+
+// client.set('ChrisAndMichael', 42, 9000);
+
+// const value = client.get('ChrisAndMichael');
+// console.log(value);
