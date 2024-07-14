9bf7b113718aec5c1bf96c3551db8ce9fc910324
michaellgans <michaellgans@Victoria>
2024-07-14 12:38:03 -0500

    M - 12:37 PM - added comments and tweaked returns

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,46 +5,39 @@ import sha1 from 'crypto-js/sha1';
 
 class UsersController {
   static async postNew(req, res) {
-    const userDocs = await dbClient.db.collection('users');
+    const userDocs = dbClient.db.collection('users'); // removed await
+    // Body of .json package
     const userEmail = req.body.email;
     const userPassword = req.body.password;
     const hashedPassword = sha1(userPassword);
     const newDocument = { email: userEmail, password: hashedPassword };
 
+    console.log("New Document:", newDocument);
+
     // Email is missing: 400
     if (!userEmail) {
-      res.status(400).send("Missing email");
+      return res.status(400).send("Missing email"); // added return
     }
+    // Password is missing: 400
     if (!userPassword) {
-      res.status(400).send("Missing password");
+      return res.status(400).send("Missing password"); // added return
     }
 
+    // Returns null when no record is found
     const existingRecord = await userDocs.findOne({ email: userEmail });
     console.log(existingRecord);
 
+    // User already exists: 400
     if (existingRecord !== null) {
-      res.status(400).send("Already Exists");
+      return res.status(400).send("Already Exists"); // added return
     }
 
-    const result = userDocs.insertOne(newDocument);
-    const postResponse = { id: result.insertedId, email: userEmail };
+    const result = await userDocs.insertOne(newDocument); // added await
+    const dataToSend = { id: result.insertedId, email: userEmail };
 
-    console.log(postResponse);
-    res.status(201).send(postResponse);
+    console.log(dataToSend);
+    return res.status(201).send(dataToSend); // added return
   }
 }
 
 export default UsersController;
-
-// Syntax for if logic 
-// Response method, status method, json method
-// if (!userEmail) {
-//   return res.status(400).json({ error: "Message Here" });
-// };
-
-// Mongo Methods
-// Can we access the collection by importing db.json into this
-// and calling db.nbFiles
-// .save() - saves data to MongoDB?
-// .insertOne() - inserts one object into the DB
-// .insertMany() - inserts an array of objects into the DB
